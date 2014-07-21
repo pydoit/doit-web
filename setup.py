18a5f8bdae1238536d4b5ddcac6a3c837eb0f7fd
schettino72 <schettino72@gmail.com>
2014-07-21 18:21:33 +0800

    rename to doit-web

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 from setuptools import setup
 
 setup (
-    name = 'doitjs',
+    name = 'doit-web',
     version = '0.1.dev0',
     author = 'Eduardo Naufel Schettino',
     author_email = 'schettino72@gmail.com',
-    description = 'doit tasks for javascript stuff',
-    url = 'https://github.com/schettino72/doit-js/',
+    description = 'doit tasks for web stuff',
+    url = 'https://github.com/schettino72/doit-web/',
     keywords = ['doit', 'javascript'],
     platforms = ['any'],
     license = 'MIT',
-    packages = ['doitjs'],
+    packages = ['doitweb'],
     install_requires = ['doit', 'pathlib', 'mergedict'],
     classifiers = [
         'Development Status :: 4 - Beta',
@@ -24,8 +24,8 @@ setup (
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries',
     ]
 )
